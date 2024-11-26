fb77c3350eb6083e0bbb000b523d0fee9913e277
LCh-Salgado <lilianachicas180@gmail.com>
2024-11-26 15:09:05 -0500

    try to make a location map but dont work

--- app/src/main/java/com/example/restaurantapplch/MainActivity.java
+++ app/src/main/java/com/example/restaurantapplch/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.restaurantapplch;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,13 +16,20 @@ public class MainActivity extends AppCompatActivity {
 
     int image;
     Button viewM;
+    Button addBTN;
     Intent goToM;
+    Intent mapA;
+    public static final String ADDRESS ="Baltimore"; //"geo:0,0?q=4707+Eastern+Ave%2C+Baltimore%2C+MD+21224";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewM = (Button)findViewById(R.id.viewF);
+        addBTN = (Button)findViewById(R.id.addressBTN);
+
 
     viewM.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -32,9 +40,20 @@ public class MainActivity extends AppCompatActivity {
 
         }
     });
+    addBTN.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            showMap(Uri.parse(ADDRESS));
 
+        }
+    });
 
-
-
+    }
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
